f8567b28e4c4b9504b0754c40d1a40e12d36c54e
Yuta Nakata <61623796+nukacha@users.noreply.github.com>
2020-05-04 11:00:32 +0900

    Move solutions to problems

--- problems/278.py
+++ problems/278.py
@@ -1,4 +1,3 @@
-# https://leetcode.com/explore/featured/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3316/
 from random import randint
 
 n = int(input())
--- problems/771.py
+++ problems/771.py
@@ -1,4 +1,3 @@
-# https://leetcode.com/explore/featured/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3317/
 class Solution:
     def numJewelsInStones(self, J: str, S: str) -> int:
         jewels = set(J)
