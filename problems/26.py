7214686235d2ee7a5460257e364dcb93d4e0cc0f
Yuta Nakata <61623796+nukacha@users.noreply.github.com>
2020-05-13 00:16:21 +0900

    Improve solutions

--- problems/26.py
+++ problems/26.py
@@ -3,10 +3,9 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        i = 1
-        while i < len(nums):
-            if nums[i - 1] == nums[i]:
-                nums.pop(i)
-            else:
+        i = 0
+        for j in range(1, len(nums)):
+            if nums[i] != nums[j]:
                 i += 1
-        return len(nums)
+                nums[i] = nums[j]
+        return i + 1
--- problems/977.py
+++ problems/977.py
@@ -1,9 +1,9 @@
-# https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3240/
 from typing import List
 
 
 class Solution:
     def sortedSquares(self, A: List[int]) -> List[int]:
-        A = [a ** 2 for a in A]
+        for i in range(len(A)):
+            A[i] *= A[i]
         A.sort()
         return A
